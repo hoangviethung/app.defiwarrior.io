5e6d262bb836b0324e99332fe65a359299af5f57
Hoàng Việt Hùng <hoangviet.hung.009@gmail.com>
2021-07-02 14:19:38 +0700

    update done

--- js/core.js
+++ js/core.js
@@ -1,4 +1,20 @@
-window.addEventListener("load", (e) => {});
+const fancyboxWalletConnect = () => {
+	document
+		.querySelectorAll("[popup-to='defi_wallet_connect_popup']")
+		.forEach((item) => {
+			item.addEventListener("click", (e) => {
+				$.fancybox.open({
+					src: "#defi_wallet_connect_popup",
+					type: "inline",
+					touch: false,
+				});
+			});
+		});
+};
+
+window.addEventListener("load", (e) => {
+	fancyboxWalletConnect();
+});
 
 window.addEventListener("scroll", (e) => {});
 
