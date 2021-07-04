bd7d7eb1e62f805088c17a4a9dffed16c64f2557
hoangviethung <hoangviet.hung.009@gmail.com>
2021-07-05 01:20:48 +0700

    done 80%

--- js/core.js
+++ js/core.js
@@ -1,10 +1,10 @@
 const fancyboxWalletConnect = () => {
 	document
-		.querySelectorAll("[popup-to='defi_wallet_connect_popup']")
+		.querySelectorAll("[popup-to='wallet_connect_popup']")
 		.forEach((item) => {
 			item.addEventListener("click", (e) => {
 				$.fancybox.open({
-					src: "#defi_wallet_connect_popup",
+					src: "#wallet_connect_popup",
 					type: "inline",
 					touch: false,
 				});
